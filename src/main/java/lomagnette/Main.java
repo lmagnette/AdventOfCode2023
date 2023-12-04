ede1c86726b0baa471d6a7a8e80694d1cced86bb
Loic Magnette <loicmagnette@gmail.com>
2023-12-04 21:22:26 +0100

    initial commit

---
+++ src/main/java/lomagnette/Main.java
@@ -0,0 +1,11 @@
+package be.lomagnette;
+
+/**
+ * @author lma
+ * @date 04/12/2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
